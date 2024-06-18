d5a7cdd09c332cfeb5ee342de070d5d3ab4be5d9
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2024-06-19 02:24:37 +0530

    hide restaurants for non-admin users

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -31,11 +31,6 @@ const getMenuItems = (role: string) => {
       label: <NavLink to="/">Home</NavLink>,
     },
     {
-      key: "/restaurants",
-      icon: <Icon component={foodIcon} />,
-      label: <NavLink to="/restaurants">Restaurants</NavLink>,
-    },
-    {
       key: "/products",
       icon: <Icon component={BasketIcon} />,
       label: <NavLink to="/products">Products</NavLink>,
@@ -53,6 +48,11 @@ const getMenuItems = (role: string) => {
       key: "/users",
       icon: <Icon component={UserIcon} />,
       label: <NavLink to="/users">Users</NavLink>,
+    });
+    menus.splice(2, 0, {
+      key: "/restaurants",
+      icon: <Icon component={foodIcon} />,
+      label: <NavLink to="/restaurants">Restaurants</NavLink>,
     });
     return menus;
   }
